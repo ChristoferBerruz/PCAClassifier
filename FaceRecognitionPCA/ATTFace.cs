c0368bbb9e31e2bd4f8c26a8e3609e583593714f
ChristoferBerruz <46355467+ChristoferBerruz@users.noreply.github.com>
2020-04-10 13:51:28 -0400

    Enhanced methods and properties

--- FaceRecognitionPCA/ATTFace.cs
+++ FaceRecognitionPCA/ATTFace.cs
@@ -9,22 +9,30 @@ using Mapack;
 
 namespace FaceRecognitionPCA
 {
-    class ATTFace : IComparable
+    /// <summary>
+    /// Class to encapsulate properties of an ATTFace usefukl for PCA
+    /// </summary>
+    class ATTFace : IComparable, ICloneable
     {
+
+        public readonly int WIDTH = 92;
+
+        public readonly int HEIGHT = 112;
         /// <summary>
-        /// Full PathName to retrieve the Bitmap object later for display
+        /// FileName for display
         /// </summary>
-        public String PathName { set; get; }
+        /// 
+        public String FileName { set; get; }
 
         /// <summary>
         /// Similarity score (Euclidean distance) required to evaluate similarity
         /// </summary>
-        public double Similarity { set; get; }
+        public double Closeness { set; get; }
 
         /// <summary>
-        /// Unique identifier to be able to tell whether we used it or not
+        /// Unique identifier of the person
         /// </summary>
-        public int ID { set; get; }
+        public int personID { set; get; }
 
         /// <summary>
         /// We only care about the array representation of the image
@@ -38,23 +46,16 @@ namespace FaceRecognitionPCA
         public double[] ImageVectorTransformed { set; get; }
 
 
-        /// <summary>
-        /// Multiple ATTFaces may share the same PersonIdentifier
-        /// </summary>
-        public int PersonIdentifier { set; get; }
-
-
         public ATTFace()
         {
 
         }
 
-        public ATTFace(String PathName, int ID, double[] ImageVector, int PersonIdentifier)
+        public ATTFace(String FileName, int personID, double[] ImageVector)
         {
-            this.PathName = PathName;
-            this.ID = ID;
+            this.FileName = FileName;
+            this.personID = personID;
             this.ImageVector = ImageVector;
-            this.PersonIdentifier = PersonIdentifier;
         }
 
 
@@ -68,7 +69,7 @@ namespace FaceRecognitionPCA
             ATTFace OtherFace = obj as ATTFace;
             if(OtherFace != null)
             {
-                return this.CompareTo(OtherFace.Similarity);
+                return this.CompareTo(OtherFace.Closeness);
             }
             else
             {
@@ -76,19 +77,51 @@ namespace FaceRecognitionPCA
             }
         }
 
-        public int CompareTo(double otherSimilarity)
+        public int CompareTo(double otherCloseness)
         {
-            if(Similarity < otherSimilarity)
+            if(Closeness < otherCloseness)
             {
                 return -1;
             }
 
-            if(Similarity == otherSimilarity)
+            if(Closeness == otherCloseness)
             {
                 return 0;
             }
 
             return 1;
+        }
+
+
+        /// <summary>
+        /// Return original Bitmap from its image array.
+        /// </summary>
+        /// <returns></returns>
+        public Bitmap getImagesAsBmp() 
+        {
+            Bitmap bmp = new Bitmap(WIDTH, HEIGHT);
+            int k = 0; //To index the array
+            for(int i = 0; i < WIDTH; i++)
+            {
+                for(int j = 0; j < HEIGHT; j++)
+                {
+                    int pix = (int)this.ImageVector[k];
+                    bmp.SetPixel(i, j, Color.FromArgb(pix, pix, pix));
+                    k++;
+                }
+            }
+            return bmp;
+        }
+
+        public Object Clone()
+        {
+            ATTFace Copy = new ATTFace();
+            Copy.Closeness = this.Closeness;
+            Copy.ImageVector = this.ImageVector;
+            Copy.ImageVectorTransformed = this.ImageVectorTransformed;
+            Copy.personID = this.personID;
+            Copy.FileName = this.FileName;
+            return Copy;
         }
     }
 }
