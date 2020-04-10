f7d59a82cef133e2ae7340565a5ac9a0a3800a85
ChristoferBerruz <46355467+ChristoferBerruz@users.noreply.github.com>
2020-04-10 13:49:03 -0400

    Provides methods to draw on components

--- FaceRecognitionPCA/DisplayHandeler.cs
+++ FaceRecognitionPCA/DisplayHandeler.cs
@@ -7,48 +7,18 @@ using System.Threading.Tasks;
 
 namespace FaceRecognitionPCA
 {
+    /// <summary>
+    /// Provides basic display methods for front end
+    /// </summary>
     class DisplayHandeler
     {
-        public static double[] BitmapToArray(Bitmap bmp)
-        {
-            double[] arr = new double[bmp.Height * bmp.Width];
-            int k = 0;
-            for(int i = 0; i < bmp.Height; i++)
-            {
-                for(int j = 0; j < bmp.Width; j++)
-                {
-                    arr[k] = Convert.ToInt32(bmp.GetPixel(i, j).R);
-                    k++;
-                }
-            }
-            return arr;
-        }
-
         /// <summary>
-        /// This method assumes Bitmap to be symetric
+        /// Resizes a Bitmap to fit a certain component.
         /// </summary>
-        /// <param name="arr"></param>
+        /// <param name="bmp"> Bitmap to be resized.</param>
+        /// <param name="width"> Width of the component. </param>
+        /// <param name="height"> Height of the component. </param>
         /// <returns></returns>
-        public static Bitmap ArrayToBitmap(double[] arr)
-        {
-            double max = arr.Max();
-            double min = arr.Min();
-            int n = (int)Math.Ceiling(Math.Sqrt(arr.Length));
-            Bitmap bmp = new Bitmap(n, n);
-            int k = 0;
-            for(int i = 0; i < n; i++)
-            {
-                for(int j = 0; j < n; j++)
-                {
-                    int valueOfPixel = (int)(255 * (arr[k] - min) / (max - min));
-                    bmp.SetPixel(i, j, Color.FromArgb(valueOfPixel, valueOfPixel, valueOfPixel));
-                    k++;
-                }
-            }
-            return bmp;
-            
-        }
-
         public static Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
         {
             Bitmap result = new Bitmap(width, height);
