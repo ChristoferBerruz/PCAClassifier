792fdbe7369702081ff7f7925c65ac1007ad1627
ChristoferBerruz <46355467+ChristoferBerruz@users.noreply.github.com>
2020-04-10 13:48:18 -0400

    Includes DecisionType and ClosenessMeasure [enums]

---
+++ FaceRecognitionPCA/DecisionTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceRecognitionPCA
+{
+    enum DecisionType
+    {
+        ClossestNeighbor = 0,
+        KNN = 1
+    }
+
+    enum ClossenessMeasure
+    {
+        Euclidean = 0,
+        Projection = 1
+    }
+}
